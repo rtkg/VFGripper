8bd04b98b8f3a20509af9b865ac0c6938b87f074
Robert Krug <robert.krug@oru.se>
2014-05-13 13:45:17 +0200

    first commit

---
+++ VFGripper.ino
@@ -0,0 +1,12 @@
+//Low-level Control for the Velvet Fingers Gripper
+//Robert Krug, Todor Stoyanov 13/06/2014
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

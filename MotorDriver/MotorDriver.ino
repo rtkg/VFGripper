65d6ea5b551a9715b0e1f451cbb0b4513bd3d583
Robert Krug <robert.krug@oru.se>
2014-05-14 11:34:01 +0200

    added motor driver example

---
+++ MotorDriver/MotorDriver.ino
@@ -0,0 +1,43 @@
+//Test for the Motor Driver
+//Robert Krug, Todor Stoyanov 13/06/2014
+
+int M1_IN1=24; //Motor input 1
+int M1_IN2=25; //Motor input 2
+int M1_SF=26; // Motor status flag
+int M1_D2=6;  // PWM pin
+
+int count = 0;
+ 
+void setup() {
+  pinMode(M1_IN1,OUTPUT);
+  pinMode(M1_IN2,OUTPUT);
+  pinMode(M1_SF,INPUT);
+  pinMode(M1_D2,OUTPUT);
+  
+  analogWriteResolution(12); //sets the resolution of the analogWrite(...) function to 12 bit, i.e., between 0 - 4095
+}
+
+void loop() 
+{
+  float control_value=200;
+  
+  //M1_IN1 and M1_IN2 set the direction
+  if (count%2 == 0)
+  {
+    digitalWrite(M1_IN1,LOW);
+    digitalWrite(M1_IN2,HIGH); 
+  }
+  else
+  {
+    digitalWrite(M1_IN1,HIGH);
+    digitalWrite(M1_IN2,LOW);  
+  }  
+  
+  analogWrite(M1_D2, map(control_value, 0, 1023, 0, 4095)); //map the control value in the PWM range
+  Serial.print(" , 12-bit PWM value : ");
+  Serial.print(map(control_value, 0, 1023, 0, 4095));
+  
+  delay(2000); //wait 2 seconds
+  
+  count++;
+}

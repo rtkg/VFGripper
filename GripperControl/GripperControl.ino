493d35d205cc7eb5e1c9f5a20779e1a036bdcfa5
Robert Krug <robert.krug@oru.se>
2014-05-15 11:52:16 +0200

    MotorControl: changed some variables to float

--- GripperControl/GripperControl.ino
+++ GripperControl/GripperControl.ino
@@ -3,66 +3,71 @@
 
 //======================= Struct Definitions ======================
 struct MotorControlPins{
-  int IN1; //Motor input 1 pin
-  int IN2; //Motor input 2 pin
-  int SF;  //Motor status flag pin
-  int EN;  //Driver board enable pin
-  int FB;  //Analog input pin for current sensing 
-  int D2;  //PWM pin
+  int IN1_; //Motor input 1 pin
+  int IN2_; //Motor input 2 pin
+  int SF_;  //Motor status flag pin
+  int EN_;  //Driver board enable pin
+  int FB_;  //Analog input pin for current sensing 
+  int D2_;  //PWM pin
+
+  MotorControlPins(int IN1, int IN2, int SF, int EN, int FB, int D2) : IN1_(IN1), IN2_(IN2), SF_(SF), EN_(EN), FB_(FB), D2_(D2) {};
 };
 
 struct SensorPins{
-  int D;  //Sensor data pin
-  int CK; //Clock pin
-  int S;  //Select pin
+  int D_;  //Sensor data pin
+  int CK_; //Clock pin
+  int S_;  //Select pin
 };
 
 struct PIDParameters{
-  int Kp; 
-  int Kd;
-  int Ki;
-  int u_max; //Maximum controller output (<= max PWM)
-  int u_min; //Minimum controller output [>= -(max PWM)]
-  int I;     //Serves as memory for the integral term [i.e., I=dT*(Ki*e_0, ... , Ki*e_t)]
+  float Kp_; 
+  float Kd_;
+  float Ki_;
+  float u_max_; //Maximum controller output (<= max PWM)
+  float u_min_; //Minimum controller output [>= -(max PWM)]
+  float I_;     //Serves as memory for the integral term [i.e., I=dT*(Ki*e_0, ... , Ki*e_t)]
+  
+  PIDParameters(float Kp, float Kd, float Ki, float u_max, float u_min, float I) : Kp_(Kp), Kd_(Kd), Ki_(Ki), u_max_(u_max), u_min_(u_min), I_(I) {};
 };
 
 struct SensorStates
 {
-  int v;  //sensor value
-  int dv; //time-derivative of the sensor value
+  float v_;  //sensor value
+  float dv_; //time-derivative of the sensor value
+  
+  SensorStates(float v, float dv) : v_(v), dv_(dv) {};
 };
 //======================= Global Variables ========================
-int dT = 1000; //Sample time in microseconds
-MotorControlPins m1_pins={24, 25, 26, 27, A0, 6};     //Motor 1 Arduino pins
-PIDParameters pid_m1_pc={0, 0, 0, 4095, -4095, 4095, 0}; //Position controller PID parameters for Motor 1
-SensorStates e1_s={0 ,0}; //Sensor states for encoder 1
+float dT = 1000; //Sample time in microseconds
+MotorControlPins* m1_pins=new MotorControlPins(24, 25, 26, 27, A0, 6);     //Motor 1 Arduino pins
+PIDParameters* pid_m1_pc=new PIDParameters(0.0, 0.0, 0.0, 4095.0, -4095.0, 0.0); //Position controller PID parameters for Motor 1
+SensorStates* e1_s=new SensorStates(0.0 ,0.0); //Sensor states for encoder 1
 
-int m1_pc_r=0; //Motor 1 position controller setpoint 
+float m1_pc_r=0.0; //Motor 1 position controller setpoint 
 
 //======================= Initialization ==========================
 void setup() {
-  pinMode(m1_pins.IN1,OUTPUT);
-  pinMode(m1_pins.IN2,OUTPUT);
-  pinMode(m1_pins.SF,INPUT);
-  pinMode(m1_pins.FB,INPUT);
-  pinMode(m1_pins.EN,OUTPUT);
-  pinMode(m1_pins.D2,OUTPUT);
-  
-  m1_pins.IN1=24;
+  pinMode((*m1_pins).IN1_,OUTPUT);
+  pinMode((*m1_pins).IN2_,OUTPUT);
+  pinMode((*m1_pins).SF_,INPUT);
+  pinMode((*m1_pins).FB_,INPUT);
+  pinMode((*m1_pins).EN_,OUTPUT);
+  pinMode((*m1_pins).D2_,OUTPUT);
   
   analogWriteResolution(12); //sets the resolution of the analogWrite(...) function to 12 bit, i.e., between 0 - 4095
   analogReadResolution(12); //sets the resolution of the analogRead(...) function to 12 bit, i.e., between 0 - 4095
   
   Serial.begin(9600); //open a serial connection
   
-  digitalWrite(m1_pins.EN,HIGH); //Enable the driver board
+  digitalWrite((*m1_pins).EN_,HIGH); //Enable the driver board
 
 }
 //====================  Function Declarations ===========================
 void update(); //Simple state machine which reads sensors, computes and sets controls
-int pid(int e, int de, PIDParameters* p); //Computes the controls from error e, error derivative de, and controller parameters p)
-int actuate(int control, const MotorControlPins* mc_pins); //Send the sign-corrected input to the actuator and read the motor status flag
+float pid(float e, float de, PIDParameters* p); //Computes the controls from error e, error derivative de, and controller parameters p)
+int actuate(float control, const MotorControlPins* mc_pins); //Send the sign-corrected input to the actuator and read the motor status flag
 void read(SensorStates* s, SensorPins* s_pins);
+float minimumJerk(float t0, float t, float q0, float qf); //evaluates a minimum-jerk position trajectory
 //============================== Loop ===================================
 void loop() 
 {
@@ -75,52 +80,57 @@ void loop()
   //TODO: write stuff to the output serial connection ... could be done with a lower frequency or only when triggering a request
 
 }
-//====================  Function Implementations ===========================
+//====================  Function Implementations =========================
 void update()
 {
 
 }
 //--------------------------------------------------------------------------
-int pid(int e, int de, PIDParameters* p)
+float pid(float e, float de, PIDParameters* p)
 {
-  (*p).I+=(*p).Ki*e; //update the integral term
-  int u=(*p).Kp*e+(*p).Kd*de+(*p).I; //compute the control value
+  (*p).I_+=(*p).Ki_*e; //update the integral term
+  float u=(*p).Kp_*e+(*p).Kd_*de+(*p).I_; //compute the control value
 
-  //clamp the control value and the integral term (the latter to avoid reset windup)
-  if (u > (*p).u_max)
+  //clamp the control value and back-calculate the integral term (the latter to avoid windup)
+  if (u > (*p).u_max_)
     {
-      (*p).I-=u-(*p).u_max;
-      u=(*p).u_max;
+      (*p).I_-=u-(*p).u_max_;
+      u=(*p).u_max_;
     }
-  else if(u < (*p).u_min)
+  else if(u < (*p).u_min_)
     {
-      (*p).I+=(*p).u_min-u;
-      u=(*p).u_min;
+      (*p).I+=(*p).u_min_-u;
+      u=(*p).u_min_;
     }
 
   return u;
 }
 //--------------------------------------------------------------------------
-int actuate(int control, const MotorControlPins* mc_pins)
+int actuate(float control, const MotorControlPins* mc_pins)
 {
   //adjust direction depending on the control sign 
   if (control < 0)
     {
-    digitalwrite((*mc_pins).IN1,HIGH);
-    digitalWrite(*(mc_pins).IN2,LOW);
+    digitalwrite((*mc_pins).IN1_,HIGH);
+    digitalWrite(*(mc_pins).IN2_,LOW);
     }
   else
     {
-    digitalwrite((*mc_pins).IN1,LOW);
-    digitalWrite(*(mc_pins).IN2,HIGH);
+    digitalwrite((*mc_pins).IN1_,LOW);
+    digitalWrite(*(mc_pins).IN2_,HIGH);
     }
-  analogWrite(*(mc_pins.D2), abs(control)); //set the value on the PWM
+  analogWrite(*(mc_pins.D2_), (int)(abs(control)+0.5f)); //set the value on the PWM
 
-  return digitalRead(*(mc_pins).SF); //return the motor status flag
+  return digitalRead(*(mc_pins).SF_); //return the motor status flag
 }
 //--------------------------------------------------------------------------
 void read(SensorStates* s, SensorPins* s_pins)
 {
 
+}
+//--------------------------------------------------------------------------
+float minimumJerk(float t0, float t, float T, float q0, float qf)
+{
+  return q0+(qf-q0)*(10*pow((t-t0)/T,3)-15*pow((t-t0)/T,4)+6*pow((t-t0)/T,5));
 }
 //--------------------------------------------------------------------------

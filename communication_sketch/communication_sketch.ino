ccd701fff9ceec463ec43c3789240d6b6ca3aefc
Todor Stoyanov <todor.stoyanov@oru.se>
2014-05-15 15:59:35 +0200

    simple comm protocol

---
+++ communication_sketch/communication_sketch.ino
@@ -0,0 +1,50 @@
+#define POSITION_MODE 0
+#define CURRENT_MODE 1
+#define BELT_MODE 2
+#define NO_MODE 3
+
+void setup() {
+  // start serial port at 9600 bps:
+  Serial.begin(9600);
+}
+
+int mode = NO_MODE;
+short target_val = 0;
+  
+void loop() {
+  // put your main code here, to run repeatedly:
+  processMessage();
+  Serial.println(target_val, DEC);  
+  //Serial.println(mode, DEC);
+  delay(100);
+}
+
+void processMessage() {
+
+  byte b1,b2;
+  if(Serial.available()) {
+    char code[3];  
+    for (int i=0; i<3; i++) {
+        b1 = Serial.read();
+        code[i] = b1;
+    }
+    //Serial.println(code);
+    if(code[0] == 'P' && code[1] == 'O' && code[2] == 'S') {
+       //position mode 
+       mode = POSITION_MODE;
+       b1 = Serial.read();
+       b2 = Serial.read();
+       target_val = b2;
+       target_val = target_val << 8;
+       target_val = target_val | b1;
+       //Serial.println(b1, BIN);
+       //Serial.println(b2, BIN);
+    }
+    if(code[0] == 'N' && code[1] == 'O' && code[2] == 'N') {
+       mode = NO_MODE;
+       target_val = 0;
+    }
+  } 
+}
+
+
---
+++ simple_interface/simple.cc
@@ -0,0 +1,134 @@
+#include <cstdio>
+#include <cstring>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+#include <iostream>
+#include <fcntl.h>   // File control definitions
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <cstdlib>
+
+using namespace std;
+
+int
+set_interface_attribs (int fd, int speed, int parity)
+{
+    struct termios tty;
+    memset (&tty, 0, sizeof tty);
+    if (tcgetattr (fd, &tty) != 0)
+    {
+	printf("error %d from tcgetattr", errno);
+	return -1;
+    }
+
+    cfsetospeed (&tty, speed);
+    cfsetispeed (&tty, speed);
+
+    //8N1
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;
+   
+    /* no hardware flow control */
+    tty.c_cflag &= ~CRTSCTS;
+    /* enable receiver, ignore status lines */
+    tty.c_cflag |= CREAD | CLOCAL;
+    /* disable input/output flow control, disable restart chars */
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+    /* disable canonical input, disable echo,
+       disable visually erase chars,
+       disable terminal-generated signals */
+    tty.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+    /* disable output processing */
+    tty.c_oflag &= ~OPOST;
+    //tty.c_cc[VMIN]  = 20;
+    //tty.c_cc[VTIME] = 1;
+    
+    // no flow control
+    /*tty.c_cflag &= ~CRTSCTS;
+    tty.c_cflag |= CREAD | CLOCAL;  // turn on READ & ignore ctrl lines
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
+    tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
+    tty.c_oflag &= ~OPOST; // make raw
+    tty.c_cc[VMIN]  = 0;
+    tty.c_cc[VTIME] = 20;*/
+
+    if (tcsetattr (fd, TCSANOW, &tty) != 0)
+    {
+	printf ("error %d from tcsetattr", errno);
+	return -1;
+    }
+    return 0;
+}
+
+
+
+
+int main(int argc, char* argv[]) {
+
+
+    if(argc !=4 ) {
+	std::cerr<<"usage: "<<argv[0]<<" port_string [POS|NON] val\n";
+	return -1;
+    }
+    
+    const char* portname = argv[1];
+    char *comm = argv[2];
+    short pos = 0;
+    
+    if (strncmp(comm,"POS",3) == 0) {
+	pos = atoi(argv[3]);
+    }	
+    std::cout<<"connecting to "<<portname<<" sending "<<comm<<" "<<pos<<std::endl;
+
+    int fd = open (portname, O_RDWR | O_NOCTTY);
+    if (fd < 0)
+    {
+	printf("error %d opening %s: %s", errno, portname, strerror (errno));
+	return -1;
+    }
+    sleep(2);
+
+    set_interface_attribs (fd, B9600, 0);  // set speed to 9600 bps, 8n1 (no parity)
+    tcflush(fd, 0);
+
+    short msg_size = sizeof(short) + 3; 
+    char *payload = new char[msg_size];
+    snprintf(payload,4,"%s",comm);
+    memcpy(payload+3,&pos,sizeof(short));
+    int wrote = write (fd, payload , msg_size);
+    if(wrote != msg_size) {
+	printf("wrote %d, error!\n",wrote);
+    }
+    printf("msg size %d, string %s\n",msg_size,payload);
+    delete [] payload;
+
+    fd_set rfds;
+    struct timeval tv;
+    int retval;
+
+    FD_ZERO(&rfds);
+    FD_SET(fd, &rfds);
+
+    /* Wait up to five seconds. */
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+
+    retval = select(fd+1, &rfds, NULL, NULL, &tv);
+    /* Don't rely on the value of tv now! */
+
+    if (retval == -1)
+	perror("select()");
+    else if (retval)
+	printf("Data is available now.\n");
+    else
+	printf("No data within five seconds.\n");
+
+    char buf [100];
+    int n = read (fd, buf, sizeof(buf));  // read up to 100 characters if ready to read
+    std::cout<<n<<" read "<<buf<<std::endl;
+
+    close(fd);
+}
